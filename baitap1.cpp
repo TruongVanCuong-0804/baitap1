961eeb8a730a66ee53cc894f5775127cad206916
TruongVanCuong-0804 <101450823+TruongVanCuong-0804@users.noreply.github.com>
2022-03-12 15:42:01 +0700

    Add files via upload

---
+++ baitap1.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    cout << "Ho va ten: Truong Van Cuong " << '\n'
+         << "MSSV: 106210118" << '\n'
+         << "Lop: 21DTCLC2";
+    return 0;
+}
